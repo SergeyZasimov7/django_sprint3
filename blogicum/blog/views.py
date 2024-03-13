b5a0f71ebadc9877288eab3071cf3e7b9f6801c6
Сергей Засимов <zasimovsergey@yandex.ru>
2024-03-13 21:09:40 +0300

    1

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,26 +1,44 @@
 from django.http import Http404
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from blog.models import Post
+from django.utils import timezone
 
-posts = Post.objects.all()
-
-posts_dict = {post['id']: post for post in posts}
+from .models import Post, Category
 
 
 def index(request):
-    return render(request, 'blog/index.html', {
-        'posts': reversed(posts)})
+    current_time = timezone.now()
+    posts = Post.objects.filter(
+        pub_date__lte=current_time,
+        is_published=True,
+        category__is_published=True
+    ).order_by('-pub_date')[:5]
+    return render(request, 'blog/index.html', {'posts': posts})
 
 
 def post_detail(request, post_id):
-    post = posts_dict.get(post_id)
-    if post is not None:
-        return render(request, 'blog/detail.html', {'post': post})
-    raise Http404(f'Post with id {post_id} not found')
+    current_time = timezone.now()
+    try:
+        post = Post.objects.get(id=post_id)
+        if (post.pub_date > current_time or
+            not post.is_published or
+                not post.category.is_published):
+            raise Http404(f'Post with id {post_id} is not available')
+    except Post.DoesNotExist:
+        raise Http404(f'Post with id {post_id} not found')
+    return render(request, 'blog/detail.html', {'post': post})
 
 
 def category_posts(request, category_slug):
-    return render(request, 'blog/category.html', {
-        'category_slug': category_slug})
+    current_time = timezone.now()
+    category = get_object_or_404(Category, slug=category_slug)
+    if not category.is_published:
+        raise Http404(f'Category with slug {category_slug} is not published')
+    posts_in_category = Post.objects.filter(
+        category=category,
+        pub_date__lte=current_time,
+        is_published=True
+    )
+    return render(request, 'blog/category.html', {'category': category,
+                                                  'posts': posts_in_category})
--- blogicum/core/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class PublishedModel(models.Model):
-    is_published = models.BooleanField(default=True)
-    created_at = models.DateTimeField()
-
-    class Meta:
-        abstract = True
